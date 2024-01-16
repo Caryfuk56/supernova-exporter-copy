c83e08a3a5599049ed0c6056b8bc3a8ba9f5c4d4
Martin Hlouch <mhlouch@nbmhlouch.ais-servis.cz>
2024-01-16 23:29:23 +0100

    fixed lines

--- typescript/src/brands/getBrandId.ts
+++ typescript/src/brands/getBrandId.ts
@@ -3,8 +3,6 @@ import { stringifyOutput } from "../../helpers";
 const getBrandId = (name: string, brands: Record<string, unknown>[]): string => {
   const brandObject = brands.filter((brand) => brand.name === name);
 
-  console.log("BRENDS" + stringifyOutput(brands))
-
   if (brandObject.length === 0) {
     console.log("ERROR: brand with name \"" + name + "\" doesn't exist id design system!");
     return "";
